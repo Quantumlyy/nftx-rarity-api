185a3b67945deeba49308683417e8511d0053250
Nejc Drobnič <ethoarx@gmail.com>
2021-10-03 19:27:12 +0200

    refactor: style api docs page

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle('NFTX Rarity')
+    .setVersion('0.0.1')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
